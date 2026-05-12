01e14dbcaf6243231500d0b010401632f6cb1b9f
paezn <npaez@colegioguanenta.edu.co>
2026-05-12 08:24:05 -0500

    Se modifico conexion usando mvc

---
+++ config/constantes.php
@@ -0,0 +1,13 @@
+<?php
+    // Parametros BD local - Constantes
+    define('USER', 'npaez'); //  Crea la constante USER con valor 'root'
+    define('PW', '12345');
+    define('HOST', 'localhost');
+    define('BD', 'centro_medico');
+
+    // Parametros BD remota (infinityfree)
+    /*define('USER', 'if0_41712633'); //  Crea la constante USER con valor 'root'
+    define('PW', '57CwSVfTrjcl');
+    define('HOST', 'sql213.infinityfree.com');
+    define('BD', 'if0_41712633_centro_medico');*/
+?>
---
+++ modelo/usuario_modelo.php
@@ -0,0 +1,5 @@
+<?php
+    require_once "../config/conexion.php";
+    
+    conectar();
+?>
